8af715bf31d773948b824c2e5659ffe460f7c4d8
LAPTOP-F6DO73JI\peter <sobe@htw-dresden.de>
2024-07-04 15:00:27 +0200

    added documentation,

--- MyTowerdefense/Enemy.java
+++ MyTowerdefense/Enemy.java
@@ -2,9 +2,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Enemy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * basic enemy
+ * @author (peter) 
+ * @version (0.96)
  */
 public class Enemy extends Actor
 {
@@ -14,8 +14,11 @@ public class Enemy extends Actor
      */
     public void act()
     {
-        
+        /*
+           enemy move 2 steps rightwards
+        */
         // Add your action code here.
+        this.move(2); 
     
     }
 }
